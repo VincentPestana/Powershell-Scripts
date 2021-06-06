e8cb9b89b50ff0d6efbbcc0d4bc9105dc53f47dc
VincentPestana <crashoveride001@gmail.com>
2021-06-06 19:24:21 +0200

    Calling system command

--- MSIAfterburnerAutoApply.ps1
+++ MSIAfterburnerAutoApply.ps1
@@ -7,5 +7,9 @@ Do {
     $currentHourOfDay = (Get-Date).Hour
     if (($currentHourOfDay -gt $StartApplying) -or ($currentHourOfDay -lt $StopApplying)) {
         # Apply profile
+        # Execute command
+        & "'" + $MSIPath "'" + $MSIProfile
+        "Applied"
+        Start-Sleep -Seconds 10
     }
 } While ($true)
