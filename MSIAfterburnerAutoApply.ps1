1183c49c3929d30d35d4cc9f7683926a908d5c38
VincentPestana <crashoveride001@gmail.com>
2021-06-06 18:58:45 +0200

    Logic for time

--- MSIAfterburnerAutoApply.ps1
+++ MSIAfterburnerAutoApply.ps1
@@ -1,7 +1,11 @@
 ﻿$MSIPath = ""
+$StartApplying = 23
+$StopApplying = 6
 
 Do {
-    // Get the time
+    # Get the time
     $currentHourOfDay = (Get-Date).Hour
-
+    if (($currentHourOfDay -gt $StartApplying) -or ($currentHourOfDay -lt $StopApplying)) {
+        # Apply profile
+    }
 } While ($true)
